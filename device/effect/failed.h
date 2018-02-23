e7d2c715e302e935fe3c439c3d1cb15c1613102b
Arkadiusz Buras <macbury@gmail.com>
2018-02-23 20:01:12 +0100

    Effect for failure

--- device/effect/failed.h
+++ device/effect/failed.h
@@ -4,22 +4,16 @@
 
 const int ringWidth = 6;
 const RGB failedMinColor = { 255, 60, 30 };
-const RGB failedMaxColor = { 226, 54, 2 };
-const RGB emptyColor = { 0, 0, 0 };
-
-typedef struct {
-  byte level;
-  byte power;
-  float step;
-  float alpha;
-} FailLine;
+const RGB failedMaxColor = { 188, 47, 2 };
 
 class FailedEffect : public Effect {
   public:
     virtual void update() {
-      _time += 0.01f;
-      for (byte i = 0; i < size(); i++) {
-        float alpha = 0.5 + sin(i + _time) * 0.5f;
+      _time += 0.05f;
+      for (int i = 0; i < size(); i++) {
+        int level = i / ringWidth;
+        float alpha = 0.5f + sin(level + _time) * 0.5f;
+
         matrix[i] = lerpColor(alpha, failedMinColor, failedMaxColor);
       }
     }
